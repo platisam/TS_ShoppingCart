a25fd958e4d22dbeae5c3a4ab78dde2cfdd88a9f
platisam <platisam@gmail.com>
2023-10-07 21:31:19 +0200

    importing BrowserRouter, Routes, Route and Container

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+
 function App() {
-  return <h1>TS_ShoppingCart</h1>;
+  return (
+    <Container>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/store" element={<Store />}></Route>
+        <Route path="/about" element={<About />}></Route>
+      </Routes>
+    </Container>
+  );
 }
 
 export default App;
