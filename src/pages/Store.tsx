49a06befc676ea0bf69c9fb70f59c338ad7b8b4e
platisam <platisam@gmail.com>
2023-10-09 17:47:35 +0200

    creating and adding StoreItem component to store page

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,13 +1,16 @@
 import { Col, Row } from "react-bootstrap";
 import storeItems from "../data/item.json";
+import { StoreItem } from "../components/StoreItem";
 
 export function Store() {
   return (
     <>
       <h1>Store</h1>
-      <Row>
+      <Row md={2} xs={1} lg={3} className="g-3">
         {storeItems.map((item) => (
-          <Col>{JSON.stringify(item)}</Col>
+          <Col key={item.id}>
+            <StoreItem {...item} />
+          </Col>
         ))}
       </Row>
     </>
