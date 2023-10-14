08e771817ff950ff9f5eaab925a3e9a140dc754a
platisam <platisam@gmail.com>
2023-10-14 13:01:41 +0200

    adding functionality to open and close the cart

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -1,8 +1,14 @@
 import { Offcanvas } from "react-bootstrap";
+import { useShoppingCart } from "../context/ShoppingCartContext";
 
-export function ShoppingCart() {
+type ShoppingCartProps = {
+  isOpen: boolean;
+};
+
+export function ShoppingCart({ isOpen }: ShoppingCartProps) {
+  const { closeCart } = useShoppingCart();
   return (
-    <Offcanvas show={true} placement="end">
+    <Offcanvas show={isOpen} onHide={closeCart} placement="end">
       <Offcanvas.Header closeButton>
         <Offcanvas.Title>Cart</Offcanvas.Title>
       </Offcanvas.Header>
--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -92,7 +92,7 @@ export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
       }}
     >
       {children}
-      <ShoppingCart />
+      <ShoppingCart isOpen={isOpen} />
     </ShoppingCartContext.Provider>
   );
 }
